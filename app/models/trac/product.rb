a7e9a0233c0d05d4ee10657f693ed270c3ba6cdb
Sa-ryong Kang <saryong@seedtime.co>
2014-09-11 21:34:18 +0900

    add advertiser and product model

---
+++ app/models/trac/product.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: trac_products
+#
+#  id            :integer          not null, primary key
+#  advertiser_id :integer
+#  product_code  :string(255)
+#  product_name  :string(255)
+#  product_url   :string(255)
+#  image_url     :string(255)
+#  price         :integer
+#  slug          :string(255)
+#  deleted_at    :datetime
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
+class Trac::Product < ActiveRecord::Base
+  belongs_to :advertiser
+end
