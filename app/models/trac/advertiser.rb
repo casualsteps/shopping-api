46c2dcdd61788bdddedd65877b2b355468fa12fc
Sa-ryong Kang <saryong@seedtime.co>
2014-09-11 21:34:18 +0900

    add advertiser and product model

---
+++ app/models/trac/advertiser.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: trac_advertisers
+#
+#  id                        :integer          not null, primary key
+#  advertiser_name           :string(255)
+#  advertiser_address        :string(255)
+#  advertiser_zipcode        :string(9)
+#  advertiser_telephone_no   :string(14)
+#  advertiser_login_id       :string(255)
+#  advertiser_login_password :string(255)
+#  advertiser_api_key        :string(255)
+#  advertiser_url            :string(255)
+#  deleted_at                :datetime
+#  created_at                :datetime
+#  updated_at                :datetime
+#
+
+class Trac::Advertiser < ActiveRecord::Base
+  column_prefix "advertiser_"
+
+end
---
+++ db/migrate/20140911115715_create_trac_tables.rb
@@ -0,0 +1,32 @@
+class CreateTracTables < ActiveRecord::Migration
+  def change
+    create_table :trac_products do |t|
+      t.belongs_to :advertiser, index: true
+      t.string :product_code
+      t.string :product_name
+      t.string :product_url
+      t.string :image_url
+      t.integer :price
+      t.string :slug
+      t.datetime :deleted_at
+
+      t.timestamps
+    end
+    add_index :trac_products, :product_code, unique: true
+    add_index :trac_products, :product_name
+
+    create_table :trac_advertisers do |t|
+      t.string :advertiser_name
+      t.string :advertiser_address
+      t.string :advertiser_zipcode,       limit: 9
+      t.string :advertiser_telephone_no,  limit: 14
+      t.string :advertiser_login_id
+      t.string :advertiser_login_password
+      t.string :advertiser_api_key
+      t.string :advertiser_url
+      t.datetime :deleted_at
+
+      t.timestamps
+    end
+  end
+end
