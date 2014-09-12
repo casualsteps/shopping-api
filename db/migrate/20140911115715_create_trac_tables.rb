9e119a97561d6e96a2516047c66415bb21e305a4
Sa-ryong Kang <saryong@seedtime.co>
2014-09-13 00:07:37 +0900

    modify relation tables

--- db/migrate/20140911115715_create_trac_tables.rb
+++ db/migrate/20140911115715_create_trac_tables.rb
@@ -2,6 +2,7 @@ class CreateTracTables < ActiveRecord::Migration
   def change
     create_table :trac_products do |t|
       t.belongs_to :advertiser, index: true
+
       t.string :product_code
       t.string :product_name
       t.string :product_url
@@ -14,6 +15,28 @@ class CreateTracTables < ActiveRecord::Migration
     end
     add_index :trac_products, :product_code, unique: true
     add_index :trac_products, :product_name
+
+    create_table :trac_categories_products do |t|
+      t.references  :product,  index: true
+      t.references  :category, index: true
+
+      t.datetime :deleted_at
+
+      t.timestamps
+    end
+
+    create_table :trac_categories do |t|
+      t.belongs_to  :advertiser,  index: true
+      t.belongs_to  :parent_category, index: true
+
+      t.string   :category_code
+      t.string   :category_name
+      t.datetime :deleted_at
+
+      t.timestamps
+    end
+    add_index :trac_categories, :category_code, unique: true
+    add_index :trac_categories, :category_name
 
     create_table :trac_advertisers do |t|
       t.string :advertiser_name
@@ -29,41 +52,23 @@ class CreateTracTables < ActiveRecord::Migration
       t.timestamps
     end
         
-    create_table :trac_categories_products do |t|
-      t.belongs_to  :trac_products, index: true
-      t.belongs_to  :trac_categories
-      
-      t.integer :product_id,    null: false,   default: 0 
-      t.integer :category_id,   null: false,   default: 0 
-      t.datetime :deleted_at
-      t.timestamps
-    end
-        
-    create_table :trac_categories do |t|
-      t.belongs_to  :trac_advertisers,  index: true
-      t.belongs_to  :trac_categories
-      
-      t.string  :category_code
-      t.datetime  :deleted_at
-      t.timestamps  
-    end
-    
     create_table  :trac_publishers  do |t|
       t.string  :publisher_name
       t.string  :publisher_address
-      t.string  :publisher_zipcode
-      t.string  :publisher_telephone_no
+      t.string  :publisher_zipcode,       limit: 9
+      t.string  :publisher_telephone_no,  limit: 14
       t.string  :publisher_login_id
       t.string  :publisher_login_password
       t.string  :publisher_api_key
       t.string  :publisher_url
-      t.datetime  :deleted_at  
+      t.datetime :deleted_at
+
       t.timestamps
     end
     
     create_table  :trac_offers  do |t|
-      t.belongs_to  :trac_advertisers,  index: true
-      t.belongs_to  :trac_products,     index: true
+      t.belongs_to  :advertiser,  index: true
+      t.belongs_to  :product,     index: true
       
       t.string    :offer_name
       t.string    :offer_description
@@ -72,22 +77,23 @@ class CreateTracTables < ActiveRecord::Migration
       
       t.timestamps
     end
-    
     add_index :trac_offers, :offer_name
 
-    create_table :trac_offers_publisher   do |t|
-      t.belongs_to  :trac_publishers,   index: true
-      t.belongs_to  :trac_offers,       index: true
+    create_table :trac_offers_publishers   do |t|
+      t.belongs_to  :publisher,   index: true
+      t.belongs_to  :offer,       index: true
       
       t.datetime    :deleted_at
+
       t.timestamps
     end
     
     create_table  :trac_advertisers_offers  do |t|
-      t.belongs_to  :trac_advertisers,  index: true
-      t.belongs_to  :trac_offers,       index: true
+      t.belongs_to  :advertiser,  index: true
+      t.belongs_to  :offer,       index: true
       
       t.datetime  :deleted_at
+
       t.timestamps
     end
 
